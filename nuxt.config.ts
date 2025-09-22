216d76a99c13c82fd07888402111aa9aa8df3e6c
theLazyBoy_mmTu <114607201+zanzandora@users.noreply.github.com>
2025-09-22 14:36:40 +0700

    feat: setup fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/image', '@nuxt/ui'],
+  modules: ['@nuxt/eslint', '@nuxt/image', '@nuxt/ui', '@nuxt/fonts'],
   css: ['~/assets/css/main.css'],
   eslint: {
     config: {
@@ -13,5 +13,15 @@ export default defineNuxtConfig({
   },
   vite: {
     plugins: [tailwindcss()],
+  },
+  fonts: {
+    families: [
+      // Google Fonts
+      { name: 'Montserrat', provider: 'google' },
+    ],
+    defaults: {
+      weights: [400, 600, 700], // Tải các weight mặc định
+      subsets: ['latin'], // Subset mặc định
+    },
   },
 })
